c7113994fe3e41c8068f4be4bdcfc2302ec05e54
MGanesh09 <mannenaganesh09@gmail.com>
2026-07-18 21:19:16 +0530

    Fix: Polyfill global.crypto for MongoDB driver compatibility

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,8 @@
+// Polyfill global crypto for MongoDB/dependencies that expect it
+if (!global.crypto) {
+  global.crypto = require('crypto');
+}
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
